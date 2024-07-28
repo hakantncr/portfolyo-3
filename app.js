38e6145bc0a5b52fcee5056ee3a4a16050f6cbbe
Hakan <hakantuncer.software@gmail.com>
2024-07-28 17:45:39 +0300

    /

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElement = document.querySelectorAll('.hidden');
 hiddenElement.forEach((el) => observer.observe(el));
+
+
+
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show-2');
+        } else {
+            entry.target.classList.remove('show-2');
+        }
+    });
+});
+
+const slideElement = document.querySelectorAll('.slide');
+hiddenElement.forEach((el) => observer.observe(el));
